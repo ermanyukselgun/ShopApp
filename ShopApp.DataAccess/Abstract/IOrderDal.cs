554382f64f50756646cfac769fb2b020c29ccbdb
Erman Yükselgün <erman.yukselgun@eltturkey.com>
2021-06-07 02:07:06 +0300

    .

--- ShopApp.DataAccess/Abstract/IOrderDal.cs
+++ ShopApp.DataAccess/Abstract/IOrderDal.cs
@@ -2,15 +2,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ShopApp.DataAccess.Abstract
 {
-    public interface IProductDal: IRepository<Product>
+    public interface IOrderDal:IRepository<Order>
     {
-        IEnumerable<Product> GetPopularProduct();
-
+            
     }
 }
